d860691aba68b3e6728a38f496d3b471df7356b8
javawithlwl <javawithlwl@gmail.com>
2023-09-23 07:49:03 +0530

    Added while loop examples

--- src/main/java/com/careerit/cj/day5/GeneratePrimeInRange.java
+++ src/main/java/com/careerit/cj/day5/GeneratePrimeInRange.java
@@ -2,10 +2,7 @@ package com.careerit.cj.day5;
 
 public class GeneratePrimeInRange {
 
-
     public static void main(String[] args) {
-
-
         int lb = 1;
         int ub = 50;
         for (int num = lb; num <= ub; num++) {
@@ -13,7 +10,6 @@ public class GeneratePrimeInRange {
                 System.out.print(num+" ");
             }
         }
-
     }
     public static boolean isPrime(int num){
         if(num < 2 || (num % 2 == 0 && num != 2)){
