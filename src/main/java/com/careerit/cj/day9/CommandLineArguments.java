7f35eed8b3ff8c725f1d331e3185a0bb2426627e
javawithlwl <javawithlwl@gmail.com>
2023-09-27 07:16:40 +0530

    Added array examples

---
+++ src/main/java/com/careerit/cj/day9/CommandLineArguments.java
@@ -0,0 +1,8 @@
+package com.careerit.cj.day9;
+
+public class CommandLineArguments {
+
+    public static void main(String[] args) {
+        System.out.println("Number of arguments :"+args.length);
+    }
+}
